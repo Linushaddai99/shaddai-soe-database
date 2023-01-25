3298216492a94e9ad4d1db65fe17844f0440ff4e
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-01-25 21:27:11 +0630

    Add the indexes for the foregin key

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -10,7 +10,9 @@ CREATE TABLE medical_histories (
     id SERIAL NOT NULL PRIMARY KEY,
     addmitted_at TIMESTAMP NOT NULL,
     patient_id INT NOT NULL,
-    status VARCHAR(250)
+    status VARCHAR(250),
+    constraint fK_patient_id foreign key(fK_patient_id) references 
+     patients(id)
 );
 
 CREATE TABLE treatments (
@@ -25,7 +27,7 @@ CREATE TABLE invoices (
     generated_at TIMESTAMP,
     payed_at TIMESTAMP,
     medical_histories_id INT NOT NULL,
-    constraint medical_histories_id foreign key(medical_histories_id) references medical_histories(id)
+    constraint fK_medical_histories_id foreign key(fk_medical_histories_id) references medical_histories(id)
 );
 
 CREATE TABLE invoice_items (
@@ -35,8 +37,8 @@ CREATE TABLE invoice_items (
    total_price DECIMAL NOT NULL,
    invoice_id INT,
    treatments_id INT,
-   constraint invoice_id foreign key(invoice_id) references invoices(id),
-   constraint treatments_id foreign key(treatments_id) references treatments(id),
+   constraint fk_invoice_id foreign key(fk_invoice_id) references invoices(id),
+   constraint fk_treatments_id foreign key(fk_treatments_id) references treatments(id),
 );
 
 CREATE TABLE medical_histories_treatment (
@@ -46,3 +48,11 @@ CREATE TABLE medical_histories_treatment (
     medical_histories(id), 
     constraint treatments_id foreign key(treatments_id) references treatments(id)
 );
+
+
+CREATE INDEX ON medical_histories(patient_id);
+CREATE INDEX ON invoices(medical_histories_id);
+CREATE INDEX ON invoice_items(invoice_id);
+CREATE INDEX ON invoice_items(treatments_id);
+CREATE INDEX ON medical_histories_treatment(medical_histories_id);
+CREATE INDEX ON medical_histories_treatment(treatments_id);
